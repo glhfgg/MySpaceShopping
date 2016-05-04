60b9af39f6849e75da8be5cdb99407aa145cd982
AstureHero <670054910@qq.com>
2016-05-04 09:57:31 +0800

    wjf 5/4   test

--- app/src/main/java/com/jhy/myspaceshopping/myspaceshopping/activity/MainActivity.java
+++ app/src/main/java/com/jhy/myspaceshopping/myspaceshopping/activity/MainActivity.java
@@ -72,6 +72,10 @@ public class MainActivity extends FragmentActivity {
            }
     }
 
+
+
+
+
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
