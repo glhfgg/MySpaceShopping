0dec561f936d5974d1c9ea79f0a75d5c007dc2b0
glhfgg <291977221@qq.com>
2016-05-05 17:25:41 +0800

    扫描二维码和fragment的修改

--- app/src/main/java/com/jhy/myspaceshopping/myspaceshopping/fragment/BannerFragment.java
+++ app/src/main/java/com/jhy/myspaceshopping/myspaceshopping/fragment/BannerFragment.java
@@ -1,9 +1,10 @@
 package com.jhy.myspaceshopping.myspaceshopping.fragment;
 
-import android.app.Fragment;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
